8975f54b56d90d16aa4eaae755116a1dba142fe7
Nima Sarang <nimasarang@gmail.com>
2021-06-23 12:26:28 -0400

    add net to ultis in ts

--- backpack/library/tianshou/utils/__init__.py
+++ backpack/library/tianshou/utils/__init__.py
@@ -1,8 +1,10 @@
 from tianshou.utils.config import tqdm_config
 from tianshou.utils.statistics import MovAvg, RunningMeanStd
 from tianshou.utils.log_tools import BasicLogger, LazyLogger, BaseLogger
+from tianshou.utils import net as networks
 
 __all__ = [
+    "networks",
     "MovAvg",
     "RunningMeanStd",
     "tqdm_config",
